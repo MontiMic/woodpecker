0ec3b7de3e641c5e21f6485ff859de472b4ea65a
Michele <michele.monti7@studio.unibo.it>
2026-04-20 15:11:39 +0200

    style: changed the random icon from circle to shuffle

--- client/src/Components/Board.tsx
+++ client/src/Components/Board.tsx
@@ -396,8 +396,12 @@ export default function Board() {
               title="Random puzzle"
               disabled={isLoadingNewPuzzle}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mx-auto" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M 4 9 L 8 9 C 12 9, 12 15, 16 15 L 19 15" />
+                <polyline points="18,13 21,15 18,17" />
+
+                <path d="M 4 15 L 8 15 C 12 15, 12 9, 16 9 L 19 9" />
+                <polyline points="18,7 21,9 18,11" />
               </svg>
             </button>
             <button
